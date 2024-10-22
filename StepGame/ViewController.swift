c3d51f3d7ac1d891026c1283b750f71f9459f1a2
Tyler Eaden <tyray402@yahoo.com>
2024-10-22 16:40:05 -0600

    Initial Commit

---
+++ StepGame/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  StepGame
+//
+//  Created by Tyler Eaden on 10/22/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
